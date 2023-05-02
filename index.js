9886277a4693f4743ef631958579b158faf64443
Gustavo Lopez <gustavoalzarate@gmail.com>
2023-05-02 10:52:12 -0600

    Se ordeno el código

--- index.js
+++ index.js
@@ -2,22 +2,23 @@
 import { saveTask, editTask, getAll  } from './helpers/task.js';
 import { TaskModel } from './model/task-model.js'
 
-const taskListHTML = document.getElementById('taskListHTML');
-const dialogTask = document.getElementById('dialogTask');
-const nameTask = document.getElementById('nameTask');
-const createIn = document.getElementById('createIn');
-const btnCloseDialog = document.getElementById('btnCloseDialog');
-const txtIdTask = document.getElementById('txtId');
-const updateTask = document.getElementById('updateTask');
-const terminateTask = document.getElementById('terminateTask');
-const newTask = document.getElementById('newTask');
-const newTaskDialog = document.getElementById('newTaskDialog');
-const btnCloseNewTaskDialog = document.getElementById('btnCloseNewTaskDialog');
-const btnSaveTask = document.getElementById('saveTask');
-const nameNewTask = document.getElementById('nameNewTask');
+const taskListHTML = document.querySelector('#taskListHTML');
+const dialogTask = document.querySelector('#dialogTask');
+const nameTask = document.querySelector('#nameTask');
+const createIn = document.querySelector('#createIn');
+const btnCloseDialog = document.querySelector('#btnCloseDialog');
+const txtIdTask = document.querySelector('#txtId');
+const updateTask = document.querySelector('#updateTask');
+const terminateTask = document.querySelector('#terminateTask');
+const newTask = document.querySelector('#newTask');
+const newTaskDialog = document.querySelector('#newTaskDialog');
+const btnCloseNewTaskDialog = document.querySelector('#btnCloseNewTaskDialog');
+const btnSaveTask = document.querySelector('#saveTask');
+const nameNewTask = document.querySelector('#nameNewTask');
 
 let TASKLIST = [];
 
+// Extrae las tareas del localstorage
 const getAllTasks = () =>{
     return TASKLIST = getAll();
 }
@@ -25,20 +26,22 @@ const getAllTasks = () =>{
 const showTasksHTML = () =>{
 
     const dataTasks = getAllTasks() || [];
-
     let HTML = "";
+
+    // Limpiamos el html previo antes de volver a mostrarlo
     cleanHTML();
 
+    // Pintamos las tareas que existen en nuestra BD Localstorage
     dataTasks.forEach(task => {
         
         HTML += `<div class="m-4 card w-80 h-48 border grid justify-items-center drop-shadow-md">
-            <div class="" >Name: ${task.name.toUpperCase()}</div>
-            <div class="" >Create In: ${task.date.substring(0,10).toUpperCase()}</div>
-            <div class="flex items-center">
-            <div class="py-4">Done:</div>
-            <div class="m-1"> ${(task.done) ? "<i class='small material-icons mt-2'> check </i>" : "<i class='small material-icons mt-2'> close </i>" }</div>
-            </div>
-            <button class="mt-4 cursor-pointer w-full rounded-full bg-gradient-to-r from-cyan-400 to-sky-900 hover:from-sky-500 hover:to-cyan-700 w-48 h-8 text-white edit-task" task-id=${task.id}>Edit Task</button>
+                    <div class="" >Name: ${task.name.toUpperCase()}</div>
+                    <div class="" >Create In: ${task.date.substring(0,10).toUpperCase()}</div>
+                    <div class="flex items-center">
+                    <div class="py-4">Done:</div>
+                    <div class="m-1"> ${(task.done) ? "<i class='small material-icons mt-2'> check </i>" : "<i class='small material-icons mt-2'> close </i>" }</div>
+                    </div>
+                    <button class="mt-4 cursor-pointer w-full rounded-full bg-gradient-to-r from-cyan-400 to-sky-900 hover:from-sky-500 hover:to-cyan-700 w-48 h-8 text-white edit-task" task-id=${task.id}>Edit Task</button>
                  </div>`;
 
     });
@@ -46,35 +49,18 @@ const showTasksHTML = () =>{
     taskListHTML.innerHTML = HTML;
 }
 
-// console.log(taskListHTML.childNodes);
-newTask.addEventListener('click', () =>  {
-    newTaskDialog.showModal();
-});
 
-
-btnSaveTask.addEventListener('click', () => {
-
-    const task = new TaskModel(new Date(), nameNewTask.value, new Date(), false);
-    if(saveTask(task)){
-        showTasksHTML();
-        newTaskDialog.close();
-        nameNewTask.value = '';
-    }
-
-});
-
-taskListHTML.addEventListener('click', (e) =>{
+// Se muestra la tarea individual en el modal
+const showTask = (e) => {
 
     if(e.target.classList.contains('edit-task')){
         const taskId = e.target.getAttribute('task-id');
         if(taskId){
             const respTask = TASKLIST.find(task => task.id === taskId)
             if(respTask){
-
                 nameTask.value = respTask.name;
                 createIn.value = respTask.date.substring(0,10);
                 txtIdTask.value = respTask.id;
-    
                 if(respTask.done){
                     terminateTask.classList.add('opacity-50', 'cursor-not-allowed');
                     updateTask.classList.add('opacity-50', 'cursor-not-allowed');
@@ -88,33 +74,14 @@ taskListHTML.addEventListener('click', (e) =>{
                     createIn.classList.remove('cursor-not-allowed');
                     nameTask.disabled = false;
                 }
-    
                 dialogTask.showModal();
             }
         }
     }
-});
-
-
-const cleanHTML = () => {
-
-    while(taskListHTML.firstChild){
-        taskListHTML.removeChild(taskListHTML.firstChild);
-    }
-
 }
 
-btnCloseDialog.addEventListener('click', () =>{
-    dialogTask.close();
-
-});
-
-btnCloseNewTaskDialog.addEventListener('click', () => {
-    newTaskDialog.close();
-})
-
-
-updateTask.addEventListener('click', () => {
+// Se actualiza una tarea
+const updateTaskBD = (e) => {
     const existTask = TASKLIST.find((task) => task.id === txtIdTask.value);
     if(existTask){
         existTask.name = nameTask.value;
@@ -123,11 +90,11 @@ updateTask.addEventListener('click', () => {
             dialogTask.close();
         }
     }
-});
+}
 
-terminateTask.addEventListener('click', () =>{
+// Se actualiza una tarea por el campo de 'done'
+const doneTask = (e) => {
     const taskExists = TASKLIST.find(task => task.id === txtIdTask.value);
-
     if(taskExists){
         taskExists.done = true;
         if(editTask(TASKLIST)){
@@ -135,10 +102,46 @@ terminateTask.addEventListener('click', () =>{
             dialogTask.close();
         }
     }
+}
 
-})
+// Se guarda la tarea en el localstorage
+const saveTaskBD = (e) => {
 
+    const task = new TaskModel(new Date(), nameNewTask.value, new Date(), false);
+    if(saveTask(task)){
+        showTasksHTML();
+        newTaskDialog.close();
+        nameNewTask.value = '';
+    }
+}
 
+// Limpia el html previo
+const cleanHTML = () => {
+    while(taskListHTML.firstChild){
+        taskListHTML.removeChild(taskListHTML.firstChild);
+    }
+}
 
+// Muestra el modal para una nueva tarea
+newTask.addEventListener('click', () =>  {
+    newTaskDialog.showModal();
+});
+
+// Cierra el modal de la tarea ya existente
+btnCloseDialog.addEventListener('click', () =>{
+    dialogTask.close();
+
+});
+
+// Cierra el modal de la nueva tarea
+btnCloseNewTaskDialog.addEventListener('click', () => {
+    newTaskDialog.close();
+});
+
+// Eventos
+taskListHTML.addEventListener('click', showTask);
+updateTask.addEventListener('click', updateTaskBD);
+terminateTask.addEventListener('click', doneTask);
+btnSaveTask.addEventListener('click', saveTaskBD);
 
 showTasksHTML();
